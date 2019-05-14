4bf884244255ed8c2587d40b2b6d3870e07f4d8c
Guy Barrand <guy.barrand@gmail.com>
2019-05-14 12:14:34 +0200

    update

---
+++ EsbRootView/iOS/Classes/GLView.mm
@@ -0,0 +1,8 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file EsbRootView.license for terms.
+
+#include "../../EsbRootView/main"
+
+#define EXLIB_APP EsbRootView
+
+#include <exlib/app/iOS/GLView.mm>
---
+++ EsbRootView/offscreen/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file EsbRootView.license for terms.
+
+#include "../EsbRootView/main"
+
+#include <exlib/app/offscreen/main_cpp>
+
+int main(int argc,char** argv) {return exlib_main<EsbRootView::context,EsbRootView::main>("EsbRootView",argc,argv);}
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype kernel
