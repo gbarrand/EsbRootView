5f5b7333fd608f49ac1a8f879638e733bef346ff
Guy Barrand <guy.barrand@gmail.com>
2020-06-27 15:20:27 +0200

    update

--- EsbRootView/Cocoa/GLView.mm
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2010, Guy Barrand. All rights reserved.
-// See the file EsbRootView.license for terms.
-
-#include "../EsbRootView/main"
-
-typedef EsbRootView::main app_main_t;
-
-#import <exlib/app/Cocoa/GLView.mm>
--- bush/setup/emsdk.sh
+++
@@ -1,4 +0,0 @@
-
-emsdk_home=/usr/local/emsdk/emsdk
-. ${emsdk_home}/emsdk_env.sh --build=Release
-
