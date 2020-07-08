2f0412a7b69ab217df53ad8cebe7fd7de2634081
Guy Barrand <guy.barrand@gmail.com>
2020-07-08 12:13:51 +0200

    update

---
+++ EsbRootView/wasm/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file EsbRootView.license for terms.
+
+#include "../EsbRootView/main"
+
+#define APP_NAME EsbRootView
+
+#include <exlib/app/wasm/main_cpp>
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype kernel
+
+//exlib__build_use skip
