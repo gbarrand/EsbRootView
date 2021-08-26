608db6a435de317437c279a8996592699e4f352e
Guy Barrand <guy.barrand@gmail.com>
2021-08-26 14:08:17 +0200

    update

--- bush/setup/cern_root.sh
+++ bush/setup/cern_root.sh
@@ -3,7 +3,8 @@ save_dir=`pwd`
 
 if [ "`uname -n | grep barrand-centos7`" != "" ] ; then
   LC_ALL="en_US.UTF-8";export LC_ALL  # for manpath used in the ROOT setup files.
-  cd /usr/local/ROOT/6.12.06/root/bin
+ #cd /usr/local/ROOT/6.12.06/root/bin
+  cd /usr/local/ROOT/6.24.00/root/bin
   
 elif [ "`uname -n | grep barrand-ubuntu`" != "" ] ; then
   cd /usr/local/ROOT/6.12.06/root/bin
